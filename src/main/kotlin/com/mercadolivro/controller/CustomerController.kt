8b90ce270d9a1fc90bdb5a6e1ce34b355dfd7847
ygor <ygorferreiradonascimentosilva@gmail.com>
2025-10-25 14:25:17 -0300

    Implementação do Post Customer e alterando o retorno da requisição

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,8 +1,13 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.model.CustomerModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -10,9 +15,23 @@ import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("customer")
 class CustomerController {
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomer(): CustomerModel {
-        return CustomerModel("1", "Gustavo", "gustavo@email.com")
+    fun getCustomer(): List<CustomerModel> {
+        return customers
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: PostCustomerRequest){
+        var id = if (customers.isEmpty()) {
+            "1"
+        } else {
+            customers.last().id.toInt() + 1
+        }.toString()
+
+        customers.add(CustomerModel(id, customer.name, customer.email))
     }
 
 }
